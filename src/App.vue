772809a30eab436e5fe9d771c81fef9f95d5621d
Mir Imad Ahmed <mirimadahmed@outlook.com>
2019-02-16 11:14:09 +0500

    basic scaffold project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <rating-input />
+  </div>
+</template>
+
+<script>
+import RatingInput from './components/RatingInput.vue'
+
+export default {
+  name: 'app',
+  components: {
+    RatingInput
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
